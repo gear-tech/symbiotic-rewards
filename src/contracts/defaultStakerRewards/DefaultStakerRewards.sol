691cea822be737f52b8186fd8bd8850567f2ef2a
Dmitry Kuzmin <dmitrykuzmin00@gmail.com>
2025-03-21 23:17:19 +0300

    relax solidity version constraint to ^0.8.25

--- src/contracts/defaultStakerRewards/DefaultStakerRewards.sol
+++ src/contracts/defaultStakerRewards/DefaultStakerRewards.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity ^0.8.25;
 
 import {IDefaultStakerRewards} from "../../interfaces/defaultStakerRewards/IDefaultStakerRewards.sol";
 import {IStakerRewards} from "../../interfaces/stakerRewards/IStakerRewards.sol";
